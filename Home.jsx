d2576041ce2d854717f68f1fc93f3090b089c08b
Kimi HSU <kimihsu0508@gmail.com>
2026-05-04 16:13:41 +0800

    Create Home component with interest categories

---
+++ Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Interest Categories</h1>
+            <ul>
+                <li>Technology</li>
+                <li>Health</li>
+                <li>Finance</li>
+                <li>Travel</li>
+                <li>Arts</li>
+                <li>Food</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Home;
