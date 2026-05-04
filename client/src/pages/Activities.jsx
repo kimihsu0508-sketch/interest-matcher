ea247fdf94d73e3ed7a3b6c5737543fb8c9c4f53
Kimi HSU <kimihsu0508@gmail.com>
2026-05-04 16:28:11 +0800

    Create Activities page component

---
+++ client/src/pages/Activities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Activities = () => {
+    return (
+        <div>
+            <h1>Activities</h1>
+            <p>This is a page for browsing and creating activities/posts.</p>
+        </div>
+    );
+};
+
+export default Activities;
