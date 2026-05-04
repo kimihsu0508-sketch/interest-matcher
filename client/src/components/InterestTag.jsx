79bb24f7accdb50d51605ecf1c0da0241b763bb6
Kimi HSU <kimihsu0508@gmail.com>
2026-05-04 16:29:09 +0800

    Add InterestTag component to display interests

---
+++ client/src/components/InterestTag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InterestTag = ({ interest }) => {
+    return (
+        <span className="interest-tag">
+            {interest}
+        </span>
+    );
+};
+
+export default InterestTag;
