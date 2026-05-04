28f1e72228983ccfef8476086e743f099a84ec43
Kimi HSU <kimihsu0508@gmail.com>
2026-05-04 16:29:29 +0800

    Add Navbar component for site navigation

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Interest Matcher</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
